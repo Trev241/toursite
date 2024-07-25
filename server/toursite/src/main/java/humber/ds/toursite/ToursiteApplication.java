9f32f98fd59746dc00f151c8975dcbf34a96d82f
dumpnotfun <dumpnotfun@gmail.com>
2024-07-25 15:09:35 -0400

    Created Java Spring Boot project

---
+++ server/toursite/src/main/java/humber/ds/toursite/ToursiteApplication.java
@@ -0,0 +1,13 @@
+package humber.ds.toursite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToursiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToursiteApplication.class, args);
+    }
+
+}
---
+++ server/toursite/src/test/java/humber/ds/toursite/ToursiteApplicationTests.java
@@ -0,0 +1,13 @@
+package humber.ds.toursite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToursiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
