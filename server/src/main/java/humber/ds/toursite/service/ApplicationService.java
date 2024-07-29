c790b88046c736a8ced41bb4146cd6817a0aa447
nguyennst <nguyennst99@gmail.com>
2024-07-28 22:26:45 -0400

    Changed the folder of Controller

--- server/src/main/java/humber/ds/toursite/service/ApplicationService.java
+++ server/src/main/java/humber/ds/toursite/service/ApplicationService.java
@@ -1,14 +1,5 @@
 package humber.ds.toursite.service;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
 public class ApplicationService {
-    @RequestMapping(value = "/status")
-    public ResponseEntity<Object> getStatus() {
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
+
 }
