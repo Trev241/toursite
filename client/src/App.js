feab021c4c525bf41db9f309dea14e2a11aa9597
nguyennst <nguyennst99@gmail.com>
2024-08-05 13:08:56 -0400

    solved the config

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,9 @@
-// App.js
-
-<<<<<<< Updated upstream
 import React, { useState } from "react";
-=======
-<<<<<<< HEAD
-import React, { useContext, useState } from "react";
-=======
-import React, { useState } from "react";
->>>>>>> f27b2f8a1517aab402d4373547c7ed8bad5fd9db
->>>>>>> Stashed changes
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useLocation,
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    useLocation,
 } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import NavbarPages from "./components/NavbarPages"; // Import NavbarPages
@@ -21,103 +11,74 @@ import Hero from "./components/Hero";
 import Places from "./components/destinations/Places";
 import AuthModal from "./components/AuthModal";
 import BookingPage from "./components/BookingPage";
-<<<<<<< Updated upstream
 import Profile from "./components/Profile"; // Import Profile
 import SignIn from "./pages/Signin";
 import { Booking } from "./components/Booking";
 
 function App() {
-=======
-<<<<<<< HEAD
-import PaymentPage from "./components/PaymentPage";
-import Profile from "./components/Profile"; // Import Profile
-import { AuthContext } from "./components/AuthProvider";
+    const [showAuthModal, setShowAuthModal] = useState(false);
+    const [email, setEmail] = useState(null); // Store email instead of username
 
-function App() {
-  const { clientId, setClientId } = useContext(AuthContext); // setting the client id
-=======
-import Profile from "./components/Profile"; // Import Profile
-import SignIn from "./pages/Signin";
-import { Booking } from "./components/Booking";
+    const handleAuthModalToggle = () => {
+        setShowAuthModal((prev) => !prev);
+    };
 
-function App() {
->>>>>>> f27b2f8a1517aab402d4373547c7ed8bad5fd9db
->>>>>>> Stashed changes
-  const [showAuthModal, setShowAuthModal] = useState(false);
-  const [email, setEmail] = useState(null); // Store email instead of username
+    const handleLoginSuccess = (email) => {
+        setEmail(email); // Update the state with the email
+        setShowAuthModal(false); // Close the authentication modal on success
+    };
 
-  const handleAuthModalToggle = () => {
-    setShowAuthModal((prev) => !prev);
-  };
+    // Create a component to conditionally render Navbar or NavbarPages
+    const ConditionalNavbar = () => {
+        const location = useLocation();
+        // Render Navbar only if the path is "/"
+        return location.pathname === "/" ? (
+            <Navbar onAuthModalToggle={handleAuthModalToggle} username={email} />
+        ) : (
+            <NavbarPages
+                onAuthModalToggle={handleAuthModalToggle}
+                username={email}
+                onLogout={() => setEmail(null)}
+                isHeader={true}
+            />
+        );
+    };
 
-  const handleLoginSuccess = (email) => {
-    setEmail(email); // Update the state with the email
-    setShowAuthModal(false); // Close the authentication modal on success
-  };
-
-  // Create a component to conditionally render Navbar or NavbarPages
-  const ConditionalNavbar = () => {
-    const location = useLocation();
-    // Render Navbar only if the path is "/"
-    return location.pathname === "/" ? (
-      <Navbar onAuthModalToggle={handleAuthModalToggle} username={email} />
-    ) : (
-      <NavbarPages
-        onAuthModalToggle={handleAuthModalToggle}
-        username={email}
-        onLogout={() => setEmail(null)}
-        isHeader={true}
-      />
+    return (
+        <Router>
+            <div className="relative">
+                <div className={showAuthModal ? "blur-sm" : ""}>
+                    {/* Conditionally render Navbar or NavbarPages */}
+                    <ConditionalNavbar />
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={
+                                <>
+                                    <Hero />
+                                    <Places />
+                                </>
+                            }
+                        />
+                        <Route path="/booking" element={<BookingPage />} />
+                        <Route path="/pay" element={<Booking />} />
+                        <Route path="/profile" element={<Profile />} />{" "}
+                        {/* Add this line */}
+                        <Route path="/signin" element={<SignIn />} />
+                    </Routes>
+                </div>
+                {showAuthModal && (
+                    <div className="fixed inset-0 z-50">
+                        <AuthModal
+                            isOpen={showAuthModal}
+                            onClose={handleAuthModalToggle}
+                            onLoginSuccess={handleLoginSuccess} // Pass handleLoginSuccess to AuthModal
+                        />
+                    </div>
+                )}
+            </div>
+        </Router>
     );
-  };
-
-  return (
-    <Router>
-      <div className="relative">
-        <div className={showAuthModal ? "blur-sm" : ""}>
-          {/* Conditionally render Navbar or NavbarPages */}
-          <ConditionalNavbar />
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <>
-                  <Hero />
-                  <Places />
-                </>
-              }
-            />
-            <Route path="/booking" element={<BookingPage />} />
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-            <Route path="/payment" element={<PaymentPage />} />
-            <Route path="/profile" element={<Profile />} />{" "}
-            {/* Add this line */}
-=======
->>>>>>> Stashed changes
-            <Route path="/pay" element={<Booking />} />
-            <Route path="/profile" element={<Profile />} />{" "}
-            {/* Add this line */}
-            <Route path="/signin" element={<SignIn />} />
-<<<<<<< Updated upstream
-=======
->>>>>>> f27b2f8a1517aab402d4373547c7ed8bad5fd9db
->>>>>>> Stashed changes
-          </Routes>
-        </div>
-        {showAuthModal && (
-          <div className="fixed inset-0 z-50">
-            <AuthModal
-              isOpen={showAuthModal}
-              onClose={handleAuthModalToggle}
-              onLoginSuccess={handleLoginSuccess} // Pass handleLoginSuccess to AuthModal
-            />
-          </div>
-        )}
-      </div>
-    </Router>
-  );
 }
 
 export default App;
